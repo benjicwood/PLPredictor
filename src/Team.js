54c22f52f3c8b59ba422883dc38007c50368f68d
benjicwood <bencharlwood@gmail.com>
2017-09-24 20:52:17 +0100

    add team file for individual team icomponents

---
+++ src/Team.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class Team extends Component {
+  render () {
+    return (
+      <tr>
+        <th>1</th>
+        <td><a title='Leicester City'>Leicester City</a></td>
+        <td>38</td>
+        <td>23</td>
+        <td>12</td>
+        <td>3</td>
+        <td>68</td>
+        <td>36</td>
+        <td>+32</td>
+        <td>81</td>
+        <td />
+      </tr>
+    );
+  }
+}
+
+/*
+<th>1</th>
+<td><a title='Leicester City'>Leicester City</a></td>
+<td>38</td>
+<td>23</td>
+<td>12</td>
+<td>3</td>
+<td>68</td>
+<td>36</td>
+<td>+32</td>
+<td>81</td>
+<td />
+</tr>
+
+*/
