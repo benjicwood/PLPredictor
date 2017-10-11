f53e3fca89b0c066079f4f1c464fa0af8cf2fa68
benjicwood <bencharlwood@gmail.com>
2017-10-11 22:56:33 +0100

    add league table to initial state

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,1 +1,30 @@
-/* REDUCER */
+import * as types from '../actions/types';
+
+export const initialState = {
+  league: [
+    {
+      'name': 'Manchester City',
+      'played': 7,
+      'gd': 20,
+      'points': 19
+    },
+    {
+      'name': 'Manchester United',
+      'played': 7,
+      'gd': 19,
+      'points': 19
+    },
+    {
+      'name': 'Tottenham Hotspur',
+      'played': 7,
+      'gd': 9,
+      'points': 14
+    },
+    {
+      'name': 'Chelsea',
+      'played': 7,
+      'gd': 6,
+      'points': 13
+    }
+  ]
+};
