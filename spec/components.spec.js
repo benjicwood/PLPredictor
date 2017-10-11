c8da7cdbef38eacc6eae2792b0118585c0207336
benjicwood <bencharlwood@gmail.com>
2017-10-11 21:58:01 +0100

    add a basic test that renders the title in Main

---
+++ spec/components.spec.js
@@ -0,0 +1,19 @@
+/* global describe it */
+
+import React from 'react';
+import { expect } from 'chai';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+import Main from '../src/Main';
+
+describe('components', () => {
+  describe('<Main />', () => {
+    it('should render the title', () => {
+      const wrapper = shallow(<Main />);
+      expect(wrapper.contains(<h1>PL Predictor!</h1>)).to.be.true;
+    });
+  });
+});
