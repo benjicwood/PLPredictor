4873849d52a263bd3d680841d672cb0941045738
benjicwood <bencharlwood@gmail.com>
2017-10-11 23:10:13 +0100

    remove league table from bulma

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,15 +5,10 @@ export default class Team extends Component {
     return (
       <tr>
         <th>1</th>
-        <td><a title='Leicester City'>Leicester City</a></td>
-        <td>38</td>
-        <td>23</td>
-        <td>12</td>
-        <td>3</td>
-        <td>68</td>
-        <td>36</td>
-        <td>+32</td>
-        <td>81</td>
+        <td><a title={this.props.name}>{this.props.name}</a></td>
+        <td>{this.props.played}</td>
+        <td>{this.props.gd}</td>
+        <td>{this.props.points}</td>
         <td />
       </tr>
     );
