a996adcaac883c38e3f667df52aa16049806ca11
benjicwood <bencharlwood@gmail.com>
2017-10-15 20:16:15 +0100

    the teams are showing!

--- dev/containers/TeamList.js
+++ dev/containers/TeamList.js
@@ -6,14 +6,9 @@ class Team extends Component {
   renderList () {
     return this.props.teams.map((team) => {
       return (
-        <tr>
-          <th>1</th>
-          <td><a title={this.props.name}>{this.props.name}</a></td>
-          <td>{this.props.played}</td>
-          <td>{this.props.gd}</td>
-          <td>{this.props.points}</td>
-          <td />
-        </tr>
+        <li key={team.id}>
+          {team.name} {team.points}
+        </li>
       );
     });
   }
