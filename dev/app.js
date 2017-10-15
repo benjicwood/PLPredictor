364df08949b88096d1aba4f9f5567a3f4d4af5cb
benjicwood <bencharlwood@gmail.com>
2017-10-15 19:48:53 +0100

    add redux promise

--- dev/app.js
+++ dev/app.js
@@ -4,6 +4,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise';
 import { createLogger } from 'redux-logger';
 
 import Main from './components/Main';
@@ -11,7 +12,7 @@ import Main from './components/Main';
 import reducer from './reducers/index';
 
 const logger = createLogger();
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger, thunk)));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(logger, thunk, promise)));
 
 ReactDOM.render(
   <Provider store={store}><Main /></Provider>,
