bb921a01ca0e5e7fa4e40be82a498a391c3e0146
benjicwood <bencharlwood@gmail.com>
2017-10-15 21:15:48 +0100

    add rest of teams to league table

--- dev/reducers/TeamsReducer.js
+++ dev/reducers/TeamsReducer.js
@@ -27,6 +27,118 @@ export default function () {
       played: 8,
       gd: 0,
       points: 15
+    },
+    {
+      id: 5,
+      name: 'Chelsea',
+      played: 8,
+      gd: 5,
+      points: 13
+    },
+    {
+      id: 6,
+      name: 'Arsenal',
+      played: 8,
+      gd: 2,
+      points: 13
+    },
+    {
+      id: 7,
+      name: 'Burnley',
+      played: 8,
+      gd: 2,
+      points: 13
+    },
+    {
+      id: 8,
+      name: 'Liverpool',
+      played: 8,
+      gd: 1,
+      points: 13
+    },
+    {
+      id: 9,
+      name: 'Newcastle United',
+      played: 8,
+      gd: 1,
+      points: 11
+    },
+    {
+      id: 10,
+      name: 'Southampton',
+      played: 8,
+      gd: -2,
+      points: 9
+    },
+    {
+      id: 11,
+      name: 'West Bromwich Albion',
+      played: 7,
+      gd: -2,
+      points: 9
+    },
+    {
+      id: 12,
+      name: 'Huddersfield Town',
+      played: 8,
+      gd: -4,
+      points: 9
+    },
+    {
+      id: 13,
+      name: 'Swansea City',
+      played: 8,
+      gd: -3,
+      points: 8
+    },
+    {
+      id: 14,
+      name: 'Brighton and Hove Albion',
+      played: 8,
+      gd: -4,
+      points: 8
+    },
+    {
+      id: 15,
+      name: 'West Ham United',
+      played: 8,
+      gd: -6,
+      points: 8
+    },
+    {
+      id: 16,
+      name: 'Everton',
+      played: 8,
+      gd: -8,
+      points: 8
+    },
+    {
+      id: 17,
+      name: 'Stoke City',
+      played: 8,
+      gd: -9,
+      points: 8
+    },
+    {
+      id: 18,
+      name: 'Leicester City',
+      played: 7,
+      gd: -3,
+      points: 5
+    },
+    {
+      id: 19,
+      name: 'Bournemouth',
+      played: 8,
+      gd: -8,
+      points: 4
+    },
+    {
+      id: 20,
+      name: 'Crystal Palace',
+      played: 8,
+      gd: -16,
+      points: 3
     }
   ];
 }
