d74e736f06ad4c7ee6edff8d76cdeeec9a5db72c
benjicwood <bencharlwood@gmail.com>
2017-10-23 23:29:51 +0100

    update league table with recent round of results

--- dev/reducers/TeamsReducer.js
+++ dev/reducers/TeamsReducer.js
@@ -3,141 +3,141 @@ export default function () {
     {
       id: 1,
       name: 'Manchester City',
-      played: 8,
-      gd: 25,
-      points: 22
+      played: 9,
+      gd: 28,
+      points: 25
     },
     {
       id: 2,
       name: 'Manchester United',
-      played: 8,
-      gd: 19,
+      played: 9,
+      gd: 18,
       points: 20
     },
     {
       id: 3,
       name: 'Tottenham Hotspur',
-      played: 8,
-      gd: 10,
-      points: 17
+      played: 9,
+      gd: 13,
+      points: 20
     },
     {
       id: 4,
       name: 'Watford',
-      played: 8,
-      gd: 0,
+      played: 9,
+      gd: -2,
       points: 15
     },
     {
       id: 5,
       name: 'Chelsea',
-      played: 8,
-      gd: 5,
-      points: 13
+      played: 9,
+      gd: 7,
+      points: 16
     },
     {
       id: 6,
       name: 'Arsenal',
-      played: 8,
-      gd: 2,
-      points: 13
+      played: 9,
+      gd: 5,
+      points: 16
     },
     {
       id: 7,
       name: 'Burnley',
-      played: 8,
-      gd: 2,
+      played: 9,
+      gd: -1,
       points: 13
     },
     {
       id: 8,
       name: 'Liverpool',
-      played: 8,
-      gd: 1,
+      played: 9,
+      gd: -2,
       points: 13
     },
     {
       id: 9,
       name: 'Newcastle United',
-      played: 8,
-      gd: 1,
-      points: 11
+      played: 9,
+      gd: 2,
+      points: 14
     },
     {
       id: 10,
       name: 'Southampton',
-      played: 8,
-      gd: -2,
-      points: 9
+      played: 9,
+      gd: -1,
+      points: 12
     },
     {
       id: 11,
       name: 'West Bromwich Albion',
-      played: 7,
-      gd: -2,
-      points: 9
+      played: 9,
+      gd: -3,
+      points: 10
     },
     {
       id: 12,
       name: 'Huddersfield Town',
-      played: 8,
-      gd: -4,
-      points: 9
+      played: 9,
+      gd: -3,
+      points: 12
     },
     {
       id: 13,
       name: 'Swansea City',
-      played: 8,
-      gd: -3,
+      played: 9,
+      gd: -4,
       points: 8
     },
     {
       id: 14,
       name: 'Brighton and Hove Albion',
-      played: 8,
-      gd: -4,
-      points: 8
+      played: 9,
+      gd: -1,
+      points: 11
     },
     {
       id: 15,
       name: 'West Ham United',
-      played: 8,
-      gd: -6,
+      played: 9,
+      gd: -9,
       points: 8
     },
     {
       id: 16,
       name: 'Everton',
-      played: 8,
-      gd: -8,
+      played: 9,
+      gd: -11,
       points: 8
     },
     {
       id: 17,
       name: 'Stoke City',
-      played: 8,
-      gd: -9,
+      played: 9,
+      gd: -10,
       points: 8
     },
     {
       id: 18,
       name: 'Leicester City',
-      played: 7,
-      gd: -3,
-      points: 5
+      played: 9,
+      gd: -2,
+      points: 9
     },
     {
       id: 19,
       name: 'Bournemouth',
-      played: 8,
-      gd: -8,
-      points: 4
+      played: 9,
+      gd: -7,
+      points: 7
     },
     {
       id: 20,
       name: 'Crystal Palace',
-      played: 8,
-      gd: -16,
+      played: 9,
+      gd: -17,
       points: 3
     }
   ];
