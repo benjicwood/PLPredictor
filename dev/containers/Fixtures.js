84befa3e938ecca2af33d99a5627f77da3a870c8
benjicwood <bencharlwood@gmail.com>
2017-10-23 23:55:43 +0100

    add 3 real fixtures

--- dev/containers/Fixtures.js
+++ dev/containers/Fixtures.js
@@ -28,7 +28,19 @@ export default class Fixtures extends Component {
             <td style={{paddingTop: '5px', paddingLeft: '40px', width: '200px'}}><p>Manchester United</p></td>
             <td style={{paddingLeft: '40px'}}><Score /></td>
             <td style={{paddingRight: '40px'}}><Score /></td>
-            <td style={{paddingTop: '5px', width: '200px'}}><p>Manchester City</p></td>
+            <td style={{paddingTop: '5px', width: '200px'}}><p>Tottenham Hotspur</p></td>
+          </tr>
+          <tr>
+            <td style={{paddingTop: '5px', paddingLeft: '40px', width: '200px'}}><p>Arsenal</p></td>
+            <td style={{paddingLeft: '40px'}}><Score /></td>
+            <td style={{paddingRight: '40px'}}><Score /></td>
+            <td style={{paddingTop: '5px', width: '200px'}}><p>Swansea City</p></td>
+          </tr>
+          <tr>
+            <td style={{paddingTop: '5px', paddingLeft: '40px', width: '200px'}}><p>Crystal Palace</p></td>
+            <td style={{paddingLeft: '40px'}}><Score /></td>
+            <td style={{paddingRight: '40px'}}><Score /></td>
+            <td style={{paddingTop: '5px', width: '200px'}}><p>West Ham United</p></td>
           </tr>
         </tbody>
       </table>
