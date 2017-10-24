6a2ba3e09887e00d1218a9c50b15782878e0bbe9
benjicwood <bencharlwood@gmail.com>
2017-10-24 17:59:27 +0100

    add entire next round of games to the fixtures schedule

--- dev/reducers/GamesReducer.js
+++ dev/reducers/GamesReducer.js
@@ -6,6 +6,69 @@ export default function () {
       homeScore: '',
       awayTeam: 'Tottenham Hotspur',
       awayScore: ''
+    },
+    {
+      id: 2,
+      homeTeam: 'Arsenal',
+      homeScore: '',
+      awayTeam: 'Swansea City',
+      awayScore: ''
+    },
+    {
+      id: 3,
+      homeTeam: 'Crystal Palace',
+      homeScore: '',
+      awayTeam: 'West Ham United',
+      awayScore: ''
+    },
+    {
+      id: 4,
+      homeTeam: 'Liverpool',
+      homeScore: '',
+      awayTeam: 'Huddersfield Town',
+      awayScore: ''
+    },
+    {
+      id: 5,
+      homeTeam: 'Watford',
+      homeScore: '',
+      awayTeam: 'Stoke City',
+      awayScore: ''
+    },
+    {
+      id: 6,
+      homeTeam: 'West Bromwich Albion',
+      homeScore: '',
+      awayTeam: 'Manchester City',
+      awayScore: ''
+    },
+    {
+      id: 7,
+      homeTeam: 'Bournemouth',
+      homeScore: '',
+      awayTeam: 'Chelsea',
+      awayScore: ''
+    },
+    {
+      id: 8,
+      homeTeam: 'Brighton and Hove Albion',
+      homeScore: '',
+      awayTeam: 'Southampton',
+      awayScore: ''
+    },
+    {
+      id: 9,
+      homeTeam: 'Leicester City',
+      homeScore: '',
+      awayTeam: 'Everton',
+      awayScore: ''
+    },
+    {
+      id: 10,
+      homeTeam: 'Burnley',
+      homeScore: '',
+      awayTeam: 'Newcastle United',
+      awayScore: ''
     }
   ];
 }
