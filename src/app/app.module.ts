c97784b3110453e0258fec30613d644b9243be94
Ben Charlwood <ben@rotacloud.com>
2019-05-03 14:00:25 +0100

    mass commit new home page and a header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { FormsModule } from '@angular/forms'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { PremierLeagueComponent } from './premier-league/premier-league.component'
+
+import { AppRoutingModule } from './app-routing.module'
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PremierLeagueComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
+
 export class AppModule { }
